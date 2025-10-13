231f24d6313410e12597890d7c4e5df014e0a7f8
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-10-14 00:17:22 +0530

    Create Doctor.kt

---
+++ BE-Hyper-main/src/main/kotlin/com/hypermind/implantcard/model/implants/Doctor.kt
@@ -0,0 +1,11 @@
+package com.hypermind.implantcard.model.implants
+
+class Doctor {
+    var doctorId: Int? = null
+    var doctorName: String? = null
+    var doctorSpecialization: String? = null
+    var signatureUrl: String? = null
+    var hospitalId: Int? = null
+    var isActive: Boolean? = true
+    var createdDate: String? = null
+}
